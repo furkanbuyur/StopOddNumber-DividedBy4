95efd230ad5fa1aa274974efacc4820369cee08e
Furkan Can Buyur <furkanbuyur@gmail.com>
2023-12-25 03:03:05 +0300

    Version1.0

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+
+    int i,total=0;
+        System.out.println("Enter a number : ");
+
+        do {
+        i = sc.nextInt();
+        if (i % 4 == 0){
+            total += i;
+        }
+    } while(i % 2 == 0 );
+
+        
+        System.out.println(total);
+    }
+}
